5fdcd64827939d8394fe987d936574bf9708ba20
Vinicius Neves <kneves.vinicius@gmail.com>
2016-09-10 17:08:24 -0300

    Draw the arena, the paddles and the ball

--- game.py
+++ game.py
@@ -8,6 +8,37 @@ FPS = 200
 WINDOW_WIDTH = 400
 WINDOW_HEIGHT = 300
 
+# Arena settings
+LINE_THICKNESS = 10
+PADDLE_SIZE = 50
+PADDLE_OFFSET = 20
+
+# RGB colors
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+
+# Draws the game arena
+def drawArena():
+    DISPLAY_SURF.fill((0, 0, 0)) # Screen background
+    # Draws the boundaries - (surface, color, ((top-cord, left-cord), (width, height)), thickness) - Half of the thickness is outside of the rectangle/window
+    pygame.draw.rect(DISPLAY_SURF, WHITE, ((0, 0), (WINDOW_WIDTH, WINDOW_HEIGHT)), LINE_THICKNESS * 2)
+    # Draws the center line
+    pygame.draw.line(DISPLAY_SURF, WHITE, (WINDOW_WIDTH / 2, 0), (WINDOW_WIDTH / 2, WINDOW_HEIGHT), LINE_THICKNESS / 4)
+
+# Draws a paddle
+def drawPaddle(paddle):
+    # Stop paddle moving too low or too high (out of boundaries)
+    if paddle.bottom > WINDOW_HEIGHT - LINE_THICKNESS:
+        paddle.bottom = WINDOW_HEIGHT - LINE_THICKNESS
+    elif paddle.top < LINE_THICKNESS:
+        paddle.top = LINE_THICKNESS;
+    # Draws the paddle rectangle
+    pygame.draw.rect(DISPLAY_SURF, WHITE, paddle)
+
+# Draws the ball
+def drawBall(ball):
+    pygame.draw.rect(DISPLAY_SURF, WHITE, ball)
+
 def main():
     pygame.init()
     global DISPLAY_SURF
@@ -15,6 +46,23 @@ def main():
     FPS_CLOCK = pygame.time.Clock()
     DISPLAY_SURF = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
     pygame.display.set_caption('Pong')
+
+    # Initial positions - Ball in the center - Paddles in the middle
+    ball_x = (WINDOW_WIDTH - LINE_THICKNESS) / 2
+    ball_y = (WINDOW_HEIGHT - LINE_THICKNESS) / 2
+    player_left_position = (WINDOW_HEIGHT - PADDLE_SIZE) / 2
+    player_right_position = (WINDOW_HEIGHT - PADDLE_SIZE) / 2
+
+    # Create rectangles (x-cord, y-cord, width, height)
+    paddle_left = pygame.Rect(PADDLE_OFFSET, player_left_position, LINE_THICKNESS, PADDLE_SIZE)
+    paddle_right = pygame.Rect(WINDOW_WIDTH - PADDLE_OFFSET - LINE_THICKNESS, player_right_position, LINE_THICKNESS, PADDLE_SIZE)
+    ball = pygame.Rect(ball_x, ball_y, LINE_THICKNESS, LINE_THICKNESS)
+
+    # Drawing objects
+    drawArena()
+    drawPaddle(paddle_left)
+    drawPaddle(paddle_right)
+    drawBall(ball)
 
     # Game main loop
     while True:
